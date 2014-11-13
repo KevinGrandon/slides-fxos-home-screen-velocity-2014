e260db5dcdeed1d6e013ee5fe5e42e1141d98d96
Kevin Grandon <kevingrandon@yahoo.com>
2014-11-13 18:00:04 +0100

    Update example code, add images to step 3 and 4 examples.

--- velocity_code/step_3/homescreen.js
+++ velocity_code/step_3/homescreen.js
@@ -8,29 +8,26 @@ function renderIcon(icon) {
 	iconMap.set(tile, icon);
 
 	var iconPath = icon.icon;
-	tile.innerHTML = `<div style="background-image: url(${iconPath});"></div>`;
+	tile.innerHTML = `
+		<div style="background-image: url(${iconPath});" class="front"></div>
+		<div class="back"></div>`;
 
 	container.appendChild(tile);
-}
-
-function shuffleDom() {
-	for (var i = container.children.length - 1; i > 0; i--) {
-		container.appendChild(container.children[Math.random() * i | 0]);
-	}
 }
 
 FxosApps.all().then(icons => {
     icons.forEach(icon => {
     	renderIcon(icon);
     });
-	shuffleDom();
-
-    // Shuffle the dom every 5s?
-    setInterval(shuffleDom, 5 * 1000);
 });
 
 window.addEventListener('click', function(e) {
 	var icon = iconMap.get(e.target);
-	icon.launch();
+	e.target.classList.add('flipped');
+
+	setTimeout(() => {
+		icon.launch();
+		e.target.classList.remove('flipped');
+	}, 2000)
 });
 
